fb262adc2ba6926e7e51e18c400e984f9880e081
Ditrex24 <vasquezsergiodg@gmail.com>
2023-10-13 23:09:10 -0400

    prueba

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -21,7 +21,7 @@ import PaymentForm from './components/PaymentForm/PaymentForm';
 import GestionUser from './views/gestionUser/GestionUser'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'proyectofinalbackend-production-ad91.up.railway.app'
+axios.defaults.baseURL = 'https://proyectofinalbackend-production-ad91.up.railway.app'
 
 
 function App() {
